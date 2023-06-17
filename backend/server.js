27657c2c74575cb4bd61ba70054e61e7ccc44be4
Bhuvanraj Somasundaram <118756855+Bhuvanraj234@users.noreply.github.com>
2023-06-17 09:53:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
     // pingTimeout: 60000,
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://main--sensational-liger-dac015.netlify.app",
     }
 });
 
